0f4a7dbc30ceafe830752013642c091f07aeaa36
RolandYambao <rolandyambao@gmail.com>
2021-12-16 14:58:43 -0800

    Updated

--- src/GhostBlog.js
+++ src/GhostBlog.js
@@ -1,6 +1,63 @@
 import React, { Component } from 'react';
 import './GhostBlog.css';
 
+
+const blogPost = {
+    category: 'Blog Post Category 1',
+    title: 'Blog Post',
+    content: 'This is Blog Post'
+};
+
+const blogPostList = [
+    {
+        category: 'Blog Post Category 2',
+        title: 'Blog Post 2',
+        content: 'This is Blog Post 2'
+    },
+    {
+        category: 'Blog Post Category 3',
+        title: 'Blog Post 3',
+        content: 'This is Blog Post 3'
+    },
+    {
+        category: 'Blog Post Category 4',
+        title: 'Blog Post 4',
+        content: 'This is Blog Post 4'
+    },
+    {
+        category: 'Blog Post Category 5',
+        title: 'Blog Post 5',
+        content: 'This is Blog Post 5'
+    },
+    {
+        category: 'Blog Post Category 6',
+        title: 'Blog Post 6',
+        content: 'This is Blog Post 6'
+    },
+];
+
+const displayBlogPost = blogPostList.map((blog, idx) => {
+    return (
+        <div class="column is-12 post">
+            <article class="columns featured">
+                <div class="column is-7 post-img ">
+                    <img src="https://cdn.emk.dev/templates/featured-image.png" alt="" />
+                </div>
+                <div class="column is-5 featured-content va">
+                    <div>
+                        <h3 class="heading post-category">{blogPostList.category}</h3>
+                        <h1 class="title post-title">{blogPostList.title}</h1>
+                        <p class="post-excerpt">{blogPostList.content}</p>
+                        <br />
+                        <a href="#" class="button is-primary">Read More</a>
+                    </div>
+
+                </div>
+            </article>
+        </div>
+    );
+});
+
 class GhostBlog extends Component {
     render() {
         return (
@@ -141,6 +198,7 @@ class GhostBlog extends Component {
                                         </div>
                                     </article>
                                 </div>
+                                {displayBlogPost}
                             </div>
                         </div>
                     </div>
