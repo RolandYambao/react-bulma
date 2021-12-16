587141dcf77ba79f3638ec255250f9caf12e7d72
RolandYambao <rolandyambao@gmail.com>
2021-12-15 16:08:24 -0800

    Updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 // Components
 import Racing from './Racing';
 import Basketball from './Basketball';
-
 import Bowling from './Bowling';
 import Running from './Running';
 import Shooting from './Shooting';
