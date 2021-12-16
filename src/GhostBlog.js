c5fb49f71a82b161a56d585b7fd6925686303551
RolandYambao <rolandyambao@gmail.com>
2021-12-16 15:03:14 -0800

    Updated

--- src/GhostBlog.js
+++ src/GhostBlog.js
@@ -5,49 +5,54 @@ import './GhostBlog.css';
 const blogPost = {
     category: 'Blog Post Category 1',
     title: 'Blog Post',
-    content: 'This is Blog Post'
+    content: 'This is Blog Post',
 };
 
 const blogPostList = [
     {
         category: 'Blog Post Category 2',
         title: 'Blog Post 2',
-        content: 'This is Blog Post 2'
+        content: 'This is Blog Post 2',
     },
     {
         category: 'Blog Post Category 3',
         title: 'Blog Post 3',
-        content: 'This is Blog Post 3'
+        content: 'This is Blog Post 3',
     },
     {
         category: 'Blog Post Category 4',
         title: 'Blog Post 4',
-        content: 'This is Blog Post 4'
+        content: 'This is Blog Post 4',
     },
     {
         category: 'Blog Post Category 5',
         title: 'Blog Post 5',
-        content: 'This is Blog Post 5'
+        content: 'This is Blog Post 5',
     },
     {
         category: 'Blog Post Category 6',
         title: 'Blog Post 6',
-        content: 'This is Blog Post 6'
+        content: 'This is Blog Post 6',
+    },
+    {
+        category: 'Blog Post Category 7',
+        title: 'Blog Post 7',
+        content: 'This is Blog Post 7',
     },
 ];
 
 const displayBlogPost = blogPostList.map((blog, idx) => {
     return (
-        <div class="column is-12 post">
-            <article class="columns featured">
-                <div class="column is-7 post-img ">
+        <div class="column is-4 post" key={idx}>
+            <article class="columns is-multiline">
+                <div class="column is-12 post-img ">
                     <img src="https://cdn.emk.dev/templates/featured-image.png" alt="" />
                 </div>
-                <div class="column is-5 featured-content va">
+                <div class="column is-12 featured-content va">
                     <div>
-                        <h3 class="heading post-category">{blogPostList.category}</h3>
-                        <h1 class="title post-title">{blogPostList.title}</h1>
-                        <p class="post-excerpt">{blogPostList.content}</p>
+                        <h3 class="heading post-category">{blog.category}</h3>
+                        <h1 class="title post-title">{blog.title}</h1>
+                        <p class="post-excerpt">{blog.content}</p>
                         <br />
                         <a href="#" class="button is-primary">Read More</a>
                     </div>
