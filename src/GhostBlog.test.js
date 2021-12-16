4cf9d9f1a45d65a25a1997fe708bcc8544e9aab9
RolandYambao <rolandyambao@gmail.com>
2021-12-16 15:05:02 -0800

    Updated

--- src/GhostBlog.test.js
+++ src/GhostBlog.test.js
@@ -12,3 +12,39 @@ test('renders Blogs Post Template text', () => {
     const pElement = screen.getByText(/This template is based off/i);
     expect(pElement).toBeInTheDocument();
 });
+
+test('renders Blogs Post Content text', () => {
+    render(<GhostBlog />);
+    const pElement = screen.getByText(/This is Blog Post 2/i);
+    expect(pElement).toBeInTheDocument();
+});
+
+test('renders Blogs Post Content text', () => {
+    render(<GhostBlog />);
+    const pElement = screen.getByText(/This is Blog Post 3/i);
+    expect(pElement).toBeInTheDocument();
+});
+
+test('renders Blogs Post Content text', () => {
+    render(<GhostBlog />);
+    const pElement = screen.getByText(/This is Blog Post 4/i);
+    expect(pElement).toBeInTheDocument();
+});
+
+test('renders Blogs Post Content text', () => {
+    render(<GhostBlog />);
+    const pElement = screen.getByText(/This is Blog Post 5/i);
+    expect(pElement).toBeInTheDocument();
+});
+
+test('renders Blogs Post Content text', () => {
+    render(<GhostBlog />);
+    const pElement = screen.getByText(/This is Blog Post 6/i);
+    expect(pElement).toBeInTheDocument();
+});
+
+test('renders Blogs Post Content text', () => {
+    render(<GhostBlog />);
+    const pElement = screen.getByText(/This is Blog Post 7/i);
+    expect(pElement).toBeInTheDocument();
+});
