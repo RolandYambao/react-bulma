ef5f45721516521abcc0aa892d9b6491e6a78651
RolandYambao <rolandyambao@gmail.com>
2021-12-16 13:47:22 -0800

    Updated

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,41 @@ test('renders dashboard tag', () => {
   const spanElement = screen.getByText(/Dashboard/i);
   expect(spanElement).toBeInTheDocument();
 });
+
+test('renders customers tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Customers/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('renders authentication tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Authentication/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('renders payments tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Payments/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+test('renders transfers tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Transfers/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+
+test('renders balance tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByText(/Balance/i);
+  expect(spanElement).toBeInTheDocument();
+});
+
+
+test('renders question tag', () => {
+  render(<Forum />);
+  const spanElement = screen.getByTitle(/Question/i);
+  expect(spanElement).toBeInTheDocument();
+});
